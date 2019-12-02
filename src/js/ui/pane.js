327d2772e31717e0aa54021c92db618daae3e74a
Konstantin Babich <k.babich95@gmail.com>
2019-12-02 17:32:54 +0200

    Press Escape key simulates Cancel button click. Moved logic to keyup, because keypress event doesn't catch Escape key.

--- src/js/ui/pane.js
+++ src/js/ui/pane.js
@@ -51,13 +51,10 @@ $(function () {
                     $popup.find("p").html(this.Description).toggle(this.Description != null);
                     $popup.find("li").remove();
 
-                    $popup.unbind().keypress(function(event) {
+                    $popup.unbind().keyup(function(event) {
                         if (event.key === "Enter")
                             $("#crm-power-pane-popup-ok") && $("#crm-power-pane-popup-ok").click();
-                    });
-
-                    $popup.unbind().keyup(function(event) {
-                        if (event.key === "Escape")
+                        else if (event.key === "Escape")
                             $("#crm-power-pane-popup-cancel") && $("#crm-power-pane-popup-cancel").click();
                     });
                     return $popup;
